c156157c7b6efca9259231087e6ca5035f55ee6c
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2019-12-30 23:40:48 -0300

    Create AveragePRsMergedBySize component

---
+++ src/components/AveragePRsMergedBySize.js
@@ -0,0 +1,57 @@
+import  React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import {getAverageTime} from '../utils/AverageTime';
+import {getPRsMerged} from '../queries/queries';
+
+
+
+const AveragePRsMergedBySize = () => {
+    const { loading, error, data } = useQuery(getPRsMerged);
+    let smallPRs = [];
+    let mediumPRs = [];
+    let largePRs = [];
+
+        let averagePRsCloseTime = '';
+
+        if (loading) {
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
+        } else if (error) {
+            return (
+                <div>
+                    ERROR!
+                </div>
+            )
+        } else if (data) {
+
+            data.repository.pullRequests.nodes.map((node) => {
+                if(node.additions + node.deletions <= 100) {
+                    smallPRs.push(node);
+                } else if(node.additions + node.deletions <= 1000) {
+                    mediumPRs.push(node);
+                } else {
+                    largePRs.push(node);
+                }
+            });
+
+            const averageSmallPRsMergeTime = getAverageTime(smallPRs);
+            const averageMediumPRsMergeTime = getAverageTime(mediumPRs);
+            const averageLargePRsMergeTime = getAverageTime(largePRs);
+
+            return (
+                <div>
+                    averageSmallPRsMergeTime: {averageSmallPRsMergeTime}
+                    <br></br>
+                    averageMediumPRsMergeTime: {averageMediumPRsMergeTime}
+                    <br></br>
+                    averageLargePRsMergeTime: {averageLargePRsMergeTime}
+                </div>
+            )
+
+        }
+}
+
+export default AveragePRsMergedBySize;
